425d36866c74f394dc616be526dd62fcd171e021
ledannyang <danny27995@gmail.com>
2019-10-07 16:20:26 +0100

    Adult content removed

--- app/src/main/java/com/example/ledannyyang/movies/MySettingFragment.kt
+++ app/src/main/java/com/example/ledannyyang/movies/MySettingFragment.kt
@@ -4,19 +4,16 @@ import android.os.Bundle
 import androidx.preference.ListPreference
 import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
-import androidx.preference.SwitchPreference
 import org.jetbrains.anko.toast
 
 class MySettingFragment : PreferenceFragmentCompat(){
     private lateinit var region: ListPreference
-    private lateinit var adultSwitch: SwitchPreference
     private lateinit var deleteCache: Preference
     private lateinit var sendFeedback: Preference
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.preference_setting, rootKey)
         region = findPreference("region") as ListPreference
-        adultSwitch = findPreference("adultContent") as SwitchPreference
         deleteCache = findPreference("cache") as Preference
         sendFeedback = findPreference("feedback") as Preference
 
@@ -25,12 +22,8 @@ class MySettingFragment : PreferenceFragmentCompat(){
             true
         }
 
-        adultSwitch.setOnPreferenceClickListener {
-            adultSwitch.context.toast(getString(R.string.restartApp))
-            true
-        }
-
         deleteCache.setOnPreferenceClickListener {
+            deleteCache.context.cacheDir.deleteRecursively()
             deleteCache.context.toast(getString(R.string.deleteCache))
             true
         }
